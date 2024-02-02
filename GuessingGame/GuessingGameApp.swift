7be912629208a37323d1ec261d5113efb3ef3d92
Benjamin Lavallee <blavallee@lcs.on.ca>
2024-02-02 08:50:43 -0500

    Initial Commit

---
+++ GuessingGame/GuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameApp.swift
+//  GuessingGame
+//
+//  Created by Benjamin Lavallee on 2024-02-02.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
